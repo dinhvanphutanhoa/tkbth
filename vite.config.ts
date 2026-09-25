93b20cb5e23ba3091db7cd7558b95b9596064798
dinhvanphutanhoa <dinhvanphutanhoa@gmail.com>
2026-09-26 06:30:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/tkbtanhoa/',
+    base: '/tkbth/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
